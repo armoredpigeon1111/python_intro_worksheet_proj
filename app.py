2ca806ecbd697e38d5425d9d47b7861533a85ef4
armoredpigeon1111 <armoredpigeon1111@outlook.com>
2021-08-09 16:03:07 -0400

    Added run_find_common_elements and find_common_elements functions

--- app.py
+++ app.py
@@ -117,4 +117,27 @@ def calculate_hundred(age):
     
     return hundred_year
 
-run_calculate_hundred()
+#run_calculate_hundred()
+
+
+
+
+
+#7 Find common elements in two different lists(no duplicates)
+
+def run_find_common_elements():
+    list_a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    list_b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    common_elem = find_common_elements(list_a, list_b)
+    print(common_elem)
+
+def find_common_elements(list_a, list_b):
+    common_elements = []
+    for item_x in list_a:
+        for item_y in list_b:   
+            if(item_x == item_y and item_x not in common_elements):
+                common_elements.append(item_x)
+        
+    return common_elements
+
+run_find_common_elements()
